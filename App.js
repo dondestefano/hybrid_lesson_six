bfe4d78148a0a31bfa5c9fd62e0509e7bb3cb0c1
dondestefano <dondestefano@users.noreply.github.com>
2021-01-17 21:39:29 +0100

    Changes ListComponent to functional component.

--- App.js
+++ App.js
@@ -1,88 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, Component, PureComponent } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, Button, TouchableOpacity } from 'react-native';
+import React, { useState, Component, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, Button, TouchableOpacity, SectionList } from 'react-native';
 
-class ListComponent extends Component {
-  state = {
-    selected: 0,
-    list: [],
-  };
+const getIntialArray = () => {
+  console.log('get initial array was called lazy way');
+  return []
+}
 
-  fetchFeed() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((json) => {
-      let dataResults = [];
-      json.forEach((item) => {
-        let result = { name: item.name, id: item.id};
-        dataResults.push(result);
+const getInitialValue = () => {
+  console.log('get initial value was called lazy way');
+  return 0
+}
+
+const ListComponent = () => {
+  const [candidateList, setCandidateList] = useState(() => getIntialArray());
+  const [selectedId, setSelectedId] = useState(() => getInitialValue());
+
+  useEffect(() => {
+    console.log("Something happened")
+    
+    if (candidateList.length === 0) {
+      console.log('Length was 0 and get feed was called');
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => {
+        let results = []; 
+        json.forEach((item) => {
+          let result = { name: item.name, id: item.id};
+          results.push(result);
+        });
+        setCandidateList(results)
+        setSelectedId(1)
       });
-      this.setState ({ list: dataResults, selected: dataResults[0].id })
-    });
-  }
+    }
+  }, [candidateList]);
 
-  componentDidMount() {
-    this.fetchFeed();
-  }
-
-  isSelected(userId) {
-    if (userId === this.state.selected) {
+  const isSelected = (userId) => { 
+    if (userId === selectedId) {
       return "orange"
     } else {
       return "white"
     }
   }
 
-  handeleOnPress(selectedContactId) {
-    this.setState({selected: selectedContactId})
-  }
-
-  handleNext(){
-    if (this.state.selected + 1 <= this.state.list.length) {
-      this.setState(
-        (prevState) => ({ selected: prevState.selected + 1 })
-      )
+  const handleNext = () =>{
+    if (selectedId + 1 <= candidateList.length) {
+      setSelectedId(selectedId + 1)
     }
   }
 
-  handlePrevious() {
-    if (this.state.selected - 1 > 0) {
-      this.setState(
-        (prevState) => ({ selected: prevState.selected - 1 })
-      )
+  const handlePrevious = () => {
+    if (selectedId - 1 > 0) {
+      setSelectedId(selectedId - 1)
     }
   }
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style= {{height: 40}}></View>
-      <FlatList style= {{height: "80%"}}
-        data={this.state.list}
-        renderItem={({ item }) =>
-        <TouchableOpacity onPress={() => this.handeleOnPress(item.id)} >
-          <View style={{backgroundColor: this.isSelected(item.id)}}>
-            <Text style={{fontSize: 18 }}>{item.name}</Text>
-          </View>
-        </TouchableOpacity>
-    
-        }
-        keyExtractor={(item) => item.name.toString()}
-      />
-      <InfoComponent userId={this.state.selected}/>
-      <View style={{flexDirection: "row", alignContent: "center"}}>
-        <Button title="Prev" onPress={() => {
-            this.handlePrevious();
+  const handleOnPress = (selectedContactId) => {
+    setSelectedId(selectedContactId)
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+    <FlatList style= {{height: "80%"}}
+      data={candidateList}
+      renderItem={({ item }) =>
+      <TouchableOpacity onPress={() => handleOnPress(item.id)} >
+        <View style={{backgroundColor: isSelected(item.id)}}>
+          <Text style={{fontSize: 18 }}>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+      }
+      keyExtractor={(item) => item.name.toString()}
+    />
+    <View style={{flexDirection: "row", alignContent: "center", margin: 12}}>
+      <Button title="Prev" onPress={() => {
+        handlePrevious();
         }} />
-        <Text>{" "+ this.state.selected + " "}</Text>
-        <Button title="Next" onPress={() => {
-            this.handleNext();
-          }} />
-      </View>
-    </SafeAreaView>     
-    )
-  }
-};
+      <Text>{" "+ selectedId + " "}</Text>
+      <Button title="Next" onPress={() => {
+        handleNext(); 
+        }} />
+    </View>
+  </SafeAreaView>     
+  )
+}
 
 class InfoComponent extends Component {
   state = {
@@ -128,7 +129,14 @@ class InfoComponent extends Component {
 }
 
 export default function App() {
-  return <ListComponent/>
+  return (
+    <View style={styles.container}>
+      <View style= {{height: 40}}></View>
+      <Text style={{fontSize: 20, fontWeight: "bold", margin: 12}}>Candidates</Text>
+      <ListComponent/>
+    </View>
+
+  ) 
 }
 
 const styles = StyleSheet.create({
